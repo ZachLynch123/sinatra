8f91a707ed23874f49f2d3f5f0f608a119d76381
Zach <zachary.lynch1337@gmail.com>
2020-08-18 19:13:44 -0700

    Created a new column in characters for foreign key

--- sinatra_project/db/schema.rb
+++ sinatra_project/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20200813043951) do
+ActiveRecord::Schema.define(version: 20200819020900) do
 
   create_table "characters", force: :cascade do |t|
     t.string  "name"
@@ -23,6 +23,7 @@ ActiveRecord::Schema.define(version: 20200813043951) do
     t.integer "intelligence"
     t.integer "wisdom"
     t.integer "charisma"
+    t.integer "user_id"
   end
 
   create_table "users", force: :cascade do |t|
